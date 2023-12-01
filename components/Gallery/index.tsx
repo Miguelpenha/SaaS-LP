227963394b7e3d762a99a6367d203642783132ac
Miguel Penha <miguel.penha1@gmail.com>
2023-12-01 00:51:49 -0300

    Ajeitando algumas coisas

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -6,7 +6,9 @@ import 'swiper/css'
 import 'swiper/css/autoplay'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-import PhotoSource from '../../public/img/Photo.png'
+import Photo1Source from '../../public/img/photos/Photo 1.jpg'
+import Photo2Source from '../../public/img/photos/Photo 2.jpg'
+import Photo3Source from '../../public/img/photos/Photo 3.jpg'
 import page from '../../services/page'
 
 function Gallery() {
@@ -26,14 +28,14 @@ function Gallery() {
             }}
         >
             <SwiperSlide>
-                <Photo placeholder="blur" src={PhotoSource} alt={page.alts.gallery[0]}/>
+                <Photo placeholder="blur" src={Photo1Source} alt={page.alts.gallery[0]}/>
             </SwiperSlide>
             {loaded && <>
                 <SwiperSlide>
-                    <Photo placeholder="blur" src={PhotoSource} alt={page.alts.gallery[1]}/>
+                    <Photo placeholder="blur" src={Photo2Source} alt={page.alts.gallery[1]}/>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Photo placeholder="blur" src={PhotoSource} alt={page.alts.gallery[2]}/>
+                    <Photo placeholder="blur" src={Photo3Source} alt={page.alts.gallery[2]}/>
                 </SwiperSlide>
             </>}
         </Container>
