fe2df646a709a87215e4ece51403492f17048b9c
Miguel Penha <miguel.penha1@gmail.com>
2023-12-01 17:53:13 -0300

    Update index.tsx

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -22,7 +22,7 @@ function About() {
                 {page.components.main.about.button.text}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25">
                     <mask id="mask0_35_3" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
-                        <rect y="0.5" width="24" height="24" fill="#D9D9D9"/>
+                        <rect y="0.5" width="24" height="24"/>
                     </mask>
                     <g mask="url(#mask0_35_3)">
                         <path d="M6.4 18.5L5 17.1L14.6 7.5H6V5.5H18V17.5H16V8.9L6.4 18.5Z" fill="white"/>
